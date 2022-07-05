c724c6e4825ea6b4c9faa7f8ce944db6fbc38de3
kernel <fangwaine@outlook.com>
2022-07-05 17:50:18 +0800

    判断一个整数是否为回文

--- src/main/java/com/wayne/IsPalindrome.java
+++ src/main/java/com/wayne/IsPalindrome.java
@@ -7,12 +7,13 @@ package com.wayne;
 public class IsPalindrome {
     public static void main(String[] args) {
         IsPalindrome isPalindrome = new IsPalindrome();
-        boolean result = isPalindrome.isPalindromeA(131);
+        boolean result = isPalindrome.isPalindromeB(10);
         System.out.println(result);
     }
 
     /**
-     * 直接使用字符串api
+     * 把输入的数字转换为字符串，直接使用StringBuilder的api翻转字符串，
+     * 对比翻转之后的字符串和输入的是否相等
      */
     public boolean isPalindrome(int x) {
         if(x < 0 ) {
@@ -24,7 +25,7 @@ public class IsPalindrome {
     }
 
     /**
-     * 对比一半的数字字符是否相等
+     * 遍历所有的字符，对比首尾字符是否相等
      */
     public boolean isPalindromeA(int x) {
         if(x < 0) {
@@ -32,6 +33,7 @@ public class IsPalindrome {
         }
         String str = String.valueOf(x);
         int length = str.length();
+        // 如果是回文的话，只需要对比一半的字符就可以知道结果
         for (int i = 0; i < length/2; i++) {
             char head = str.charAt(i);
             char tail = str.charAt(length - 1 - i);
@@ -42,4 +44,23 @@ public class IsPalindrome {
         return true;
     }
 
+    /**
+     * 翻转一半的数字，进行比较,
+     * 源数字每次去掉一位，回文数字每次增加一位
+     */
+    public boolean isPalindromeB(int x) {
+        // 负数不是回文，如果个位数是0的话，只有0才是回文
+        if(x < 0 ||(x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        // 保存翻转之后的数字
+        int reversedNum = 0;
+        while (x > reversedNum){
+            // 回文数字每次加上去掉的个位数，相应的要乘以10
+            reversedNum = reversedNum * 10 + x % 10;
+            // 源数字每次循环去掉个位数，相当于除以10
+            x /= 10;
+        }
+        return reversedNum == x || x == reversedNum/10;
+    }
 }
