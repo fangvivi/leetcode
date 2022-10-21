56302895c39f2f60ab0f4050512883b82eaa3fdb
waine <fangwaine@outlook.com>
2022-10-21 18:29:56 +0800

    爬楼梯

---
+++ src/main/java/com/wayne/ClimbingStairs.java
@@ -0,0 +1,24 @@
+package com.wayne;
+
+/**
+ * 动态规划经典题目:爬楼梯
+ * https://leetcode.cn/problems/climbing-stairs/
+ * @author wayne
+ */
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(45));
+    }
+    public static int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
