eb82c82fd8128677c83058f80d7743513354927b
fangwaine <fangwaine@outlook.com>
2023-12-25 17:11:34 +0800

    字母异位词分组

---
+++ src/main/java/com/wayne/hot100/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package com.wayne.hot100;
+
+import java.util.*;
+
+/**
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。
+ * <a href="https://leetcode.cn/problems/group-anagrams/?envType=study-plan-v2&envId=top-100-liked">链接</a>
+ *
+ * @author waine
+ * @date 2023-12-21 17:21
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+
+}
--- src/main/java/com/wayne/hot100/MoveZeroes.java
+++ src/main/java/com/wayne/hot100/MoveZeroes.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
- * https://leetcode.cn/problems/move-zeroes/?envType=study-plan-v2&envId=top-100-liked
+ * <a href="https://leetcode.cn/problems/move-zeroes/?envType=study-plan-v2&envId=top-100-liked">链接</a>
  * @author waine
  * @date 2023-12-15 13:15
  */
